c7c70d68761280108061646603e53c5d0b4cdeff
als-samara <samaraalmeida379@gmail.com>
2024-01-18 22:55:56 -0300

    Components e Pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,39 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import './App.css'
-import Footer from './components/footer/Footer'
-import NavBar from './components/navBar/NavBar'
-import Home from './pages/home/Home'
-import Login from './pages/login/Login'
+import './App.css';
+
+import Navbar from './components/navBar/NavBar';
+import Footer from './components/footer/Footer';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Login from './pages/login/Login';
 import Cadastro from './pages/cadastro/Cadastro';
-import { AuthProvider } from './contexts/AuthContext'
+import Home from './pages/home/Home';
+import { AuthProvider } from './contexts/AuthContext';
+import ListaTemas from './components/temas/listaTemas/ListaTemas';
+import FormularioTema from './components/temas/formularioTema/FormularioTema';
+import DeletarTema from './components/temas/deletarTema/DeletarTema';
+
 
 function App() {
-
   return (
     <>
-      <AuthProvider>
+    <AuthProvider>
         <BrowserRouter>
-          <NavBar />
+          <Navbar />
           <div className='min-h-[80vh]'>
             <Routes>
               <Route path="/" element={<Login />} />
               <Route path="/login" element={<Login />} />
               <Route path="/cadastro" element={<Cadastro />} />
               <Route path="home" element={<Home />} />
-
-              {/*
               <Route path="/temas" element={<ListaTemas />} />
               <Route path="/cadastroTema" element={<FormularioTema />} />
               <Route path="/editarTema/:id" element={<FormularioTema />} />
               <Route path="/deletarTema/:id" element={<DeletarTema />} />
-  */}
             </Routes>
           </div>
           <Footer />
         </BrowserRouter>
         </AuthProvider>
     </>
-  )
+  );
 }
-
-export default App
+export default App;
