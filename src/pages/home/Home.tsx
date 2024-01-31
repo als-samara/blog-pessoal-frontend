b8fd94151d914d60fb905e671c6791e98d47c777
als-samara <samaraalmeida379@gmail.com>
2024-01-31 13:59:21 -0300

    Deploy do Projeto Projeto Integrador

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -13,7 +13,7 @@ function Home() {
 
                         <div className="flex justify-around gap-4">
                             <Link to='/cadastroPostagem' className="rounded border-black border-solid border-2 px-4 py-2">
-                                Nova Postagem
+                                Ver Produtos
                             </Link>
                             {/* aqui vai o outro botão */}
                         </div>
